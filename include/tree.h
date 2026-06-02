3537c9e92d91aa61e777bbc7e651b9d909fd27f0
nastyuchin07-oss <nastyuchin07@gmail.com>
2026-06-02 12:59:13 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,42 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TREE_H_
-#define INCLUDE_TREE_H_
+#ifndef TREE_H
+#define TREE_H
 
-#endif  // INCLUDE_TREE_H_
+#include <vector>
+#include <memory>
+
+class Node {
+public:
+    char value;
+    std::vector<std::shared_ptr<Node>> children;
+    
+    Node(char val);
+};
+
+class Tree {
+private:
+    std::shared_ptr<Node> root;
+    std::vector<char> originalElements;
+    
+    void buildTree(std::shared_ptr<Node> node, std::vector<char> remainingElements);
+    void getAllPermutationsDFS(std::shared_ptr<Node> node, 
+                               std::vector<char>& current,
+                               std::vector<std::vector<char>>& result);
+    
+public:
+    Tree(const std::vector<char>& elements);
+    
+    ~Tree() = default;
+    
+    std::shared_ptr<Node> getRoot() const;
+    
+    std::vector<char> getOriginalElements() const;
+};
+
+std::vector<std::vector<char>> getAllPerms(Tree& tree);
+
+std::vector<char> getPerm1(Tree& tree, int num);
+
+std::vector<char> getPerm2(Tree& tree, int num);
+
+#endif
