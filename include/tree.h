8f54e4f6e856a357d4335f31f0d2977bac9734c9
nastyuchin07-oss <nastyuchin07@gmail.com>
2026-06-02 13:20:42 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,36 +1,34 @@
 // Copyright 2022 NNTU-CS
-#ifndef TREE_H
-#define TREE_H
+#ifndef INCLUDE_TREE_H_
+#define INCLUDE_TREE_H_
 
 #include <vector>
 #include <memory>
 
 class Node {
-public:
-    char value;
-    std::vector<std::shared_ptr<Node>> children;
-    
-    Node(char val);
+ public:
+  explicit Node(char val);
+  char value;
+  std::vector<std::shared_ptr<Node>> children;
 };
 
 class Tree {
-private:
-    std::shared_ptr<Node> root;
-    std::vector<char> originalElements;
-    
-    void buildTree(std::shared_ptr<Node> node, std::vector<char> remainingElements);
-    void getAllPermutationsDFS(std::shared_ptr<Node> node, 
-                               std::vector<char>& current,
-                               std::vector<std::vector<char>>& result);
-    
-public:
-    Tree(const std::vector<char>& elements);
-    
-    ~Tree() = default;
-    
-    std::shared_ptr<Node> getRoot() const;
-    
-    std::vector<char> getOriginalElements() const;
+ public:
+  explicit Tree(const std::vector<char>& elements);
+  ~Tree() = default;
+
+  std::shared_ptr<Node> getRoot() const;
+  std::vector<char> getOriginalElements() const;
+
+ private:
+  std::shared_ptr<Node> root_;
+  std::vector<char> original_elements_;
+
+  void buildTree(std::shared_ptr<Node> node,
+                 std::vector<char> remaining_elements);
+  void getAllPermutationsDFS(std::shared_ptr<Node> node,
+                             std::vector<char>& current,
+                             std::vector<std::vector<char>>& result);
 };
 
 std::vector<std::vector<char>> getAllPerms(Tree& tree);
@@ -39,4 +37,4 @@ std::vector<char> getPerm1(Tree& tree, int num);
 
 std::vector<char> getPerm2(Tree& tree, int num);
 
-#endif
+#endif  // INCLUDE_TREE_H_
